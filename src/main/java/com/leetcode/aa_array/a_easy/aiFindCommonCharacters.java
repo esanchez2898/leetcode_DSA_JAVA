b6bee9d1954b6c47ea8188947721a600e8fdc9cf
esanchez2898 <esanchez2898@outlook.com>
2026-06-26 00:26:31 -0600

    the Find Common Characters problem was started

--- src/main/java/com/leetcode/aa_array/a_easy/aiFindCommonCharacters.java
+++ src/main/java/com/leetcode/aa_array/a_easy/aiFindCommonCharacters.java
@@ -1,14 +1,44 @@
 package com.leetcode.aa_array.a_easy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class aiFindCommonCharacters {
 
     public static List<String> commonChars(String[] words) {
+
+        List<Map<Character, Integer>> mapList = new ArrayList<>();
+
+        for (String w : words) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i < w.length(); i++) {
+                if (map.containsKey(w.charAt(i))) {
+                    map.put(w.charAt(i), map.get(w.charAt(i)) + 1);
+                } else {
+                    map.put(w.charAt(i), 1);
+                }
+            }
+            mapList.add(map);
+        }
+
+        System.out.println(mapList);
+
+
+        for (int i = 1; i < mapList.size(); i++) {
+
+        }
+
+
+
         return null;
     }
 
     public static void main(String[] args) {
 
+        String[] words = new String[]{"bella", "label", "roller"};
+
+        commonChars(words);
     }
 }
